3f3d459e9d0cca90500bb3e7c65bcce539e2c4ed
Gigi2303 <ajibola.grace201@gmail.com>
2023-11-15 11:02:26 -0800

    commit

--- 1-entry.c
+++ 1-entry.c
@@ -73,22 +73,15 @@ int main(int argc, char **env)
 				idx++;
 			}
 			args[idx] = NULL;
-	/**	if (gb_strcmp(args[0], "setenv") == 0 && args[1] != NULL && args[2] != NULL)
-		{
-			gb_setenv(env, args[1], args[2]);
-		}else if (gb_strcmp(args[0], "unsetenv") == 0 && args[1] != NULL)
-		{
-			gb_unsetenv(env, args[1]);
-		}
-		else
-		{
-*/
+
 			success = exec_with_operator(args, env, success);
 		}
-		}
-	return_error(error_arg, line_num);
-	return (0);
 
+	}
+	
+	return_error(error_arg, line_num);
+
+	return (0);
 }
 
 
@@ -104,20 +97,12 @@ int split_cmds(char *input, char *cmd[])
 	int value = 0;/**Number of commands**/
 	char *token;
        
-	if (input == NULL || input[0] == '\0')
-		return (0);
-
-	token = gb_strtok(input, " "); /**split commands with " "**/
+	token = gb_strtok(input, ";"); /**split commands with ";"**/
 	while (token != NULL)
 	{
-		cmd[value] = gb_strdup(token);
-		if (cmd[value] == NULL)
-		{
-			perror("gb_strdup");
-			exit(EXIT_FAILURE);
-		}
+		cmd[value] = token;
 		value++;
-		token = gb_strtok(NULL, " ");
+		token = gb_strtok(NULL, ";");
 	}
 
 	return (value);
@@ -213,7 +198,7 @@ int exec_with_operator(char **args, char **env, int success)
 			return 0;
 		}
 		else
-			return (1);
+			return 1;
 	}
 
 	for (a = 0; a < ALIAS_MAX; a++)
